ca37df300b399512555f651e809cbed43da29232
rubenverkuylen <info@rubenverkuylen.nl>
2022-08-19 09:50:17 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,11 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+
+function changeColor() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = body.style.background + ";";
+}
+color1.addEventListener("input", changeColor);
+color2.addEventListener("input", changeColor);
